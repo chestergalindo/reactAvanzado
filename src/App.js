5c4d6d1fd672739cd5fff59de818035cf668caa8
cesar galindo <csgalindos@hotmail.com>
2020-04-05 23:35:46 -0500

    [class 22]
    Signed-off-by: cesar galindo <csgalindos@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './container/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './container/photoCardWithQuery'
 import { Logo } from './components/Logo'
 
-export const App = () => (
-  <div>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={2} />
-  </div>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+
+  return (
+    <div>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={2} />
+            </>
+      }
+    </div>
+  )
+}
