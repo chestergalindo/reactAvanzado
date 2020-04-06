cbe40c615db384e3088d1ca8b13cf771afbe4581
cesar galindo <csgalindos@hotmail.com>
2020-04-06 00:00:42 -0500

    [class 23]
    Signed-off-by: cesar galindo <csgalindos@hotmail.com>

--- src/container/photoCardWithQuery.js
+++ src/container/photoCardWithQuery.js
@@ -4,7 +4,7 @@ import { PhotoCard } from '../components/PhotoCard'
 import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo'
 
-const query = gql`
+const GET_SINGLE_PHOTO = gql`
 query getSinglePhoto($id:ID!) {
   photo(id:$id) {
     id
@@ -17,16 +17,18 @@ query getSinglePhoto($id:ID!) {
 }
 `
 
+const renderProps = ({ loading, error, data }) => {
+  if (loading) return <h1>Loading... </h1>
+  if (error) return <h1>error... </h1>
+  if (data) {
+    const { photo = {} } = data
+    return <PhotoCard {...photo} />
+  }
+  return null
+}
+
 export const PhotoCardWithQuery = ({ id }) => (
-  <Query query={query} variables={{ id }}>
-    {
-      ({ loading, error, data }) => {
-        if (data) {
-          const { photo = {} } = data
-          return <PhotoCard {...photo} />
-        }
-        return null
-      }
-    }
+  <Query query={GET_SINGLE_PHOTO} variables={{ id }} >
+    {renderProps}
   </Query>
 )
