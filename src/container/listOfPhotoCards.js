621c556b1396d5c922a065e15566168ac1854c97
cesar galindo <csgalindos@hotmail.com>
2020-04-05 22:02:22 -0500

    [class 21]
    Signed-off-by: cesar galindo <csgalindos@hotmail.com>

---
+++ src/container/listOfPhotoCards.js
@@ -0,0 +1,4 @@
+import { withPhotos } from '../hoc/withphotos'
+import { ListOfPhotoCardsComponents } from '../components/ListOfPhotoCards'
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponents)
